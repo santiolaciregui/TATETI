83d73103e85ff3c98bdf1aab1f403c4e190f7a8e
santiolaciregui <santiolaciregui@gmail.com>
2019-09-10 10:06:36 -0300

    holiagu

--- Organizacion de Computadoras/Proyect/TDALista.h
+++ Organizacion de Computadoras/Proyect/TDALista.h
@@ -1,81 +1,84 @@
-#ifndef ARBOL_H_INCLUDED
-#define ARBOL_H_INCLUDED
+#ifndef LISTA_H_INCLUDED
+#define LISTA_H_INCLUDED
 
-#include "lista.h"
+#define LST_ELEMENTO_NULO           1
+#define LST_NO_EXISTE_SIGUIENTE     2
+#define LST_NO_EXISTE_ANTERIOR      3
+#define LST_POSICION_INVALIDA       4
+#define LST_ERROR_MEMORIA           5
 
-#define ARB_OPERACION_INVALIDA      10
-#define ARB_POSICION_INVALIDA       11
-#define ARB_ERROR_MEMORIA           12
-
-struct nodo{
-    tElemento elemento;
-    struct nodo * padre;
-    tLista hijos;
+struct celda{
+    void * elemento;
+    struct celda * siguiente;
 };
 
-struct arbol{
-    struct nodo * raiz;
-};
-
-typedef struct nodo * tNodo;
-typedef struct arbol * tArbol;
-
+typedef struct celda * tLista;
+typedef struct celda * tPosicion;
+typedef void * tElemento;
 
 /**
-Inicializa un �rbol vac�o.
-Una referencia al �rbol creado es referenciado en *A.
+ Inicializa una lista vac�a.
+ Una referencia a la lista creada es referenciada en *L.
 **/
-extern void crear_arbol(tArbol * a);
+extern void crear_lista(tLista * l);
 
 /**
-Crea la ra�z de A.
-Si A no es vac�o, finaliza indicando ARB_OPERACION_INVALIDA.
+ Inserta el elemento E, en la posici�n P, en L.
+ Con L = A,B,C,D y la posici�n P direccionando C, luego:
+ L' = A,B,E,C,D
 **/
-extern void crear_raiz(tArbol a, tElemento e);
+extern void l_insertar(tLista l, tPosicion p, tElemento e);
 
 /**
- Inserta y retorna un nuevo nodo en A.
- El nuevo nodo se agrega en A como hijo de NP, hermano izquierdo de NH, y cuyo r�tulo es E.
- Si NH es NULL, el nuevo nodo se agrega como primer hijo de NP.
- Si NH no corresponde a un nodo hijo de NP, finaliza indicando ARB_POSICION_INVALIDA.
- NP direcciona al nodo padre, mientras NH al nodo hermano derecho del nuevo nodo a insertar.
+ Elimina la celda P de L. El elemento almacenado en la posici�n P es eliminado mediante la funci�n fEliminar parametrizada.
+ Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
 **/
-extern tNodo a_insertar(tArbol a, tNodo np, tNodo nh, tElemento e);
+extern void l_eliminar(tLista l, tPosicion p, void (*fEliminar)(tElemento));
 
 /**
- Elimina el nodo N de A. El elemento almacenado en el �rbol es eliminado mediante la funci�n fEliminar parametrizada.
- Si N es la ra�z de A, y tiene un s�lo hijo, este pasa a ser la nueva ra�z del �rbol.
- Si N es la ra�z de A, y a su vez tiene m�s de un hijo, finaliza retornando ARB_OPERACION_INVALIDA.
- Si N no es la ra�z de A y tiene hijos, estos pasan a ser hijos del padre de N, en el mismo orden y a partir de la posici�n que ocupa N en la lista de hijos de su padre.
+ Destruye la lista L, elimininando cada una de sus celdas. Los elementos almacenados en las celdas son eliminados mediante la funci�n fEliminar parametrizada.
 **/
-extern void a_eliminar(tArbol a, tNodo pa, void (*fEliminar)(tElemento));
+extern void l_destruir(tLista * l, void (*fEliminar)(tElemento));
 
-/**
- Destruye el �rbol A, elimininando cada uno de sus nodos.
- Los elementos almacenados en el �rbol son eliminados mediante la funci�n fEliminar parametrizada.
+ /**
+ Recupera y retorna el elemento en la posici�n P.
+ Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
 **/
-extern void a_destruir(tArbol * a, void (*fEliminar)(tElemento));
+extern tElemento l_recuperar(tLista l, tPosicion p);
 
 /**
-Recupera y retorna el elemento del nodo N.
-*/
-extern tElemento a_recuperar(tArbol a, tNodo n);
-
-/**
-Recupera y retorna el nodo correspondiente a la ra�z de A.
+ Recupera y retorna la primera posici�n de L.
+ Si L es vac�a, primera(L) = ultima(L) = fin(L).
 **/
-extern tNodo a_raiz(tArbol a);
+extern tPosicion l_primera(tLista l);
 
 /**
- Obtiene y retorna una lista con los nodos hijos de N en A.
+ Recupera y retorna la posici�n siguiente a P en L.
+ Si P es fin(L), finaliza indicando LST_NO_EXISTE_SIGUIENTE.
 **/
-extern tLista a_hijos(tArbol a, tNodo n);
+extern tPosicion l_siguiente(tLista l, tPosicion p);
 
 /**
- Inicializa un nuevo �rbol en *SA.
- El nuevo �rbol en *SA se compone de los nodos del sub�rbol de A a partir de N.
- El subarbol de A a partir de N debe ser eliminado de A.
+ Recupera y retorna la posici�n anterior a P en L.
+ Si P es primera(L), finaliza indicando LST_NO_EXISTE_ANTERIOR.
 **/
-extern void a_sub_arbol(tArbol a, tNodo n, tArbol * sa);
+extern tPosicion l_anterior(tLista l, tPosicion p);
 
-#endif // ARBOL_H_INCLUDED
+ /**
+ Recupera y retorna la �ltima posici�n de L.
+ Si L es vac�a, primera(L) = ultima(L) = fin(L).
+**/
+extern tPosicion l_ultima(tLista l);
+
+ /**
+ Recupera y retorna la posici�n fin de L.
+ Si L es vac�a, primera(L) = ultima(L) = fin(L).
+**/
+extern tPosicion l_fin(tLista l);
+
+/**
+ Retorna la longitud actual de la lista.
+**/
+extern int l_longitud(tLista l);
+
+#endif // LISTA_H_INCLUDED
