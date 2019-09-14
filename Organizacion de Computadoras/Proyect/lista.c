d15b0caedfe0c3b402a553eff677f8b9c8982f47
santiolaciregui <santiolaciregui@gmail.com>
2019-09-14 18:01:55 -0300

    Update lista.c

--- Organizacion de Computadoras/Proyect/lista.c
+++ Organizacion de Computadoras/Proyect/lista.c
@@ -1,19 +1,79 @@
 #include "lista.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-void crear_lista(tLista * l){
+/**
+ Inicializa una lista vac�a.
+ Una referencia a la lista creada es referenciada en *L.
+**/
+void crear_lista(tLista * l);void crear_lista(tLista * l){
     (*l)=(struct celda*)malloc(sizeof(struct celda));
     (*l)->elemento=NULL;
     (*l)->siguiente=NULL;
 }
 
+/**
+ Inserta el elemento E, en la posici�n P, en L.
+ Con L = A,B,C,D y la posici�n P direccionando C, luego:
+ L' = A,B,E,C,D
+**/
 void l_insertar(tLista l, tPosicion p, tElemento e){
     struct celda *nuevo=(struct celda*)malloc(sizeof(struct celda));
     (*nuevo).elemento=e;
     (*nuevo).siguiente=(*p).siguiente;
     (*p).siguiente=nuevo;
 }
+
+/**
+ Elimina la celda P de L. El elemento almacenado en la posici�n P es eliminado mediante la funci�n fEliminar parametrizada.
+ Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
+**/
+void l_eliminar(tLista l, tPosicion p, void (*fEliminar)(tElemento));
+
+/**
+ Destruye la lista L, elimininando cada una de sus celdas. Los elementos almacenados en las celdas son eliminados mediante la funci�n fEliminar parametrizada.
+**/
+void l_destruir(tLista * l, void (*fEliminar)(tElemento));
+
+ /**
+ Recupera y retorna el elemento en la posici�n P.
+ Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
+**/
+tElemento l_recuperar(tLista l, tPosicion p);
+
+/**
+ Recupera y retorna la primera posici�n de L.
+ Si L es vac�a, primera(L) = ultima(L) = fin(L).
+**/
+tPosicion l_primera(tLista l);
+
+/**
+ Recupera y retorna la posici�n siguiente a P en L.
+ Si P es fin(L), finaliza indicando LST_NO_EXISTE_SIGUIENTE.
+**/
+tPosicion l_siguiente(tLista l, tPosicion p);
+
+/**
+ Recupera y retorna la posici�n anterior a P en L.
+ Si P es primera(L), finaliza indicando LST_NO_EXISTE_ANTERIOR.
+**/
+tPosicion l_anterior(tLista l, tPosicion p);
+
+ /**
+ Recupera y retorna la �ltima posici�n de L.
+ Si L es vac�a, primera(L) = ultima(L) = fin(L).
+**/
+tPosicion l_ultima(tLista l);
+
+ /**
+ Recupera y retorna la posici�n fin de L.
+ Si L es vac�a, primera(L) = ultima(L) = fin(L).
+**/
+tPosicion l_fin(tLista l);
+
+/**
+ Retorna la longitud actual de la lista.
+**/
+int l_longitud(tLista l);
 
 
 int main(){
