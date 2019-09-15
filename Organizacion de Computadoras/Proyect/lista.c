932d0b01591d426e2a5f3876bad47bbd23484868
santiolaciregui <santiolaciregui@gmail.com>
2019-09-14 23:51:03 -0300

    funca

--- Organizacion de Computadoras/Proyect/lista.c
+++ Organizacion de Computadoras/Proyect/lista.c
@@ -5,7 +5,7 @@
  Inicializa una lista vac�a.
  Una referencia a la lista creada es referenciada en *L.
 **/
-void crear_lista(tLista * l);void crear_lista(tLista * l){
+void crear_lista(tLista * l){
     (*l)=(struct celda*)malloc(sizeof(struct celda));
     (*l)->elemento=NULL;
     (*l)->siguiente=NULL;
@@ -27,7 +27,8 @@ void l_insertar(tLista l, tPosicion p, tElemento e){
  Elimina la celda P de L. El elemento almacenado en la posici�n P es eliminado mediante la funci�n fEliminar parametrizada.
  Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
 **/
-void l_eliminar(tLista l, tPosicion p, void (*fEliminar)(tElemento));
+void l_eliminar(tLista l, tPosicion p, void (*fEliminar)(tElemento)){
+}
 
 /**
  Destruye la lista L, elimininando cada una de sus celdas. Los elementos almacenados en las celdas son eliminados mediante la funci�n fEliminar parametrizada.
@@ -49,20 +50,29 @@ tElemento l_recuperar(tLista l, tPosicion p){
  Si L es vac�a, primera(L) = ultima(L) = fin(L).
 **/
 tPosicion l_primera(tLista l){
+    tPosicion aux = l->siguiente;
     return l->siguiente;
 }
 /**
  Recupera y retorna la posici�n siguiente a P en L.
  Si P es fin(L), finaliza indicando LST_NO_EXISTE_SIGUIENTE.
 **/
-tPosicion l_siguiente(tLista l, tPosicion p);
+tPosicion l_siguiente(tLista l, tPosicion p){
+    if (p->siguiente==NULL)
+        exit(LST_NO_EXISTE_SIGUIENTE);
+    return p->siguiente;
+}
+
 
 /**
  Recupera y retorna la posici�n anterior a P en L.
  Si P es primera(L), finaliza indicando LST_NO_EXISTE_ANTERIOR.
 **/
-tPosicion l_anterior(tLista l, tPosicion p);
-
+tPosicion l_anterior(tLista l, tPosicion p){
+    if(p==l->siguiente)
+        exit(LST_NO_EXISTE_ANTERIOR);
+    return p;
+}
  /**
  Recupera y retorna la �ltima posici�n de L.
  Si L es vac�a, primera(L) = ultima(L) = fin(L).
@@ -98,5 +108,15 @@ int l_longitud(tLista l) {
     return contador;
 }
 
+int main(){
+    tLista l;
+    crear_lista(&l);
+    tElemento e=(tElemento) 100;
+    l_insertar(l, &(*l), e);
+    printf("%i \n", l_primera(l)->elemento);
+    printf("%i", l_longitud(l));
+    printf("ee");
+
+}
 
 
